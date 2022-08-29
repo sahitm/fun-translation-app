6fbfe9fc8d33671e036f17af14f7ee2008033a83
Sahit Mandapati <67734756+sahitm@users.noreply.github.com>
2022-08-29 11:42:49 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#text-input");
+console.log(textInput);
+var outputDiv = document.querySelector("#output")
+
+var serverURL = "https://api.funtranslations.com/translate/valyrian.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function clickEventHandler() {
+    var inputText = textInput.value
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            console.log(translatedText);
+            outputDiv.innerText = translatedText;
+        })
+}
+
+btnTranslate.addEventListener("click", clickEventHandler);
+
